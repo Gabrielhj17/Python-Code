dc3457f8e1cbbcf846740791d2ad38f7641288ca
Gabrielhj17 <gabrielhj@icloud.com>
2024-10-09 18:01:53 +0100

    Create asciiScroller.py

---
+++ asciiScroller.py
@@ -0,0 +1,14 @@
+import string, time, random
+
+letters = string.ascii_letters + " "
+target = "Hello World"
+result = ""
+
+for letter in target:
+    while True:
+        I = random.choice(letters)
+        print(result + I)
+        if I == letter:
+            result += I
+            break
+        time.sleep(0.005)
